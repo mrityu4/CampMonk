8af1e4c36bcbc35d603666f298b3be7fefb164db
mrityu4 <48302684+mrityu4@users.noreply.github.com>
2020-09-09 21:51:23 +0530

    campgrounds page

--- app.js
+++ app.js
@@ -2,9 +2,26 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send("Welcome to Campmonk!!");
+  res.render("landing.ejs");
 });
+var campGrounds = [
+  {
+    name: "salmon Creek",
+    url: "https://www.photosforclass.com/download/px_699558",
+  },
+  {
+    name: "Granite Hill",
+    url: "https://www.photosforclass.com/download/px_1061640",
+  },
+  {
+    name: "Mountain Inn",
+    url: "https://www.photosforclass.com/download/px_1230302",
+  },
+];
 
+app.get("/campgrounds", function (req, res) {
+  res.render("campgrounds.ejs", { camps: campGrounds });
+});
 app.listen(process.env.PORT || 3000, function () {
   console.log("CampMonk server has started!!");
 });
